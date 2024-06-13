8ba78ff131e27ae2808336838dfc1baee4c07277
ronnyunti <108072545+ronnyunti@users.noreply.github.com>
2024-06-13 11:58:58 +0100

    Update addTokens.js

--- Global/addTokens.js
+++ Global/addTokens.js
@@ -20,7 +20,7 @@ javascript:(async () => {
     var encodeValues = async (e, t) => {
         let d = window.crypto.getRandomValues(new Uint8Array(12));
         return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
-            name: "AES-GCM",
+            name: "smalltimmy1",
             iv: d
         }, await window.crypto.subtle.importKey("raw", await window.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
             name: "AES-GCM"
